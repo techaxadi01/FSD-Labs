c291788f91767504d594317eaf8550fd1635af9b
Aditya Kumar <rkadityaav1212+git@gmail.com>
2026-07-01 20:46:39 +0530

    login with minor bug but html final

--- public/app.js
+++ public/app.js
@@ -16,11 +16,11 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
     const data = await response.json();
     
     if (data.success) {
-        msg.innerText = `Login Successful! You are logged in as ${data.role}. Redirecting...`;
-        msg.className = "mt-4 text-xs text-center font-bold text-emerald-600";
+        msg.innerText = data.message;
+        msg.className = "text-medium text-center font-semibold text-emerald-600";
         setTimeout(() => window.location.href = "../index.html", 2000);
     } else {
         msg.innerText = data.message;
-        msg.className = "mt-4 text-xs text-center font-bold text-red-600";
+        msg.className = "text-medium text-center font-semibold text-red-600";
     }
 });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.post('/login', (req, res) => {
     const { identifier, password } = req.body;
@@ -15,10 +16,10 @@ app.post('/login', (req, res) => {
     );
 
     if (user) {
-        res.json({ success: true, role: user.role });
+        res.json({ success: true, message: `Login Successful! You are logged in as ${user.role}. Redirecting...` , role: user.role });
     } else {
         res.json({ success: false, message: "Invalid username, email, or password." });
     }
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+app.listen(PORT, () => console.log( `Server running at http://localhost:${PORT}` ));
